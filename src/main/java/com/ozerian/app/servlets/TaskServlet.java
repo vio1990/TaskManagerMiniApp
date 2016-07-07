9dcc2d5e24db74d41897f47d5ff6a73adb802aef
vio1990 <vio1990@ukr.net>
2016-07-07 22:55:47 +0300

    Initial commit.

---
+++ src/main/java/com/ozerian/app/servlets/TaskServlet.java
@@ -0,0 +1,19 @@
+package com.ozerian.app.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "TaskServlet")
+public class TaskServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/tasklist.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Task Manager</title>
+</head>
+<body>
+<h1>TODO List</h1>
+</body>
+</html>
