f8ae1f90168d4c0de48c86dc6031400578972626
Ivan Ozerian <vio1990@ukr.net>
2016-07-08 11:03:30 +0300

    Addition of javadoc.

--- src/main/java/com/ozerian/app/servlets/TaskServlet.java
+++ src/main/java/com/ozerian/app/servlets/TaskServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Servlet for treatment of user's input data.
+ */
 @WebServlet(name = "TaskServlet", urlPatterns = {"/taskServlet"})
 public class TaskServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
